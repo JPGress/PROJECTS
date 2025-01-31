9bde08c5a084a1126783be6e91e4907207cb6058
JPGress <jpgress@gmail.com>
2025-01-31 11:30:45 -0300

    Change header and menu colors in 0wL.sh for enhanced visibility

--- PORTFOLIO/OWL-SH/0wL.sh
+++ PORTFOLIO/OWL-SH/0wL.sh
@@ -40,18 +40,18 @@
                 #! ╚██████╔╝╚███╔███╔╝███████╗    ██║     ███████╗██║ ╚████║   ██║   ███████╗███████║   ██║       ███████║╚██████╗██║  ██║██║██║        ██║   
                 #!  ╚═════╝  ╚══╝╚══╝ ╚══════╝    ╚═╝     ╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚══════╝   ╚═╝       ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝   
         echo
-        echo -e "\t\t ${GREEN}${BG_BLACK} ██████╗ ██╗    ██╗██╗        ██████╗ ██████╗ ███████╗ ${RESET}"
-        echo -e "\t\t ${GREEN}${BG_BLACK}██╔═████╗██║    ██║██║       ██╔═══██╗██╔══██╗██╔════╝ ${RESET}"
-        echo -e "\t\t ${GREEN}${BG_BLACK}██║██╔██║██║ █╗ ██║██║       ██║   ██║██████╔╝███████╗ ${RESET}"
-        echo -e "\t\t ${GREEN}${BG_BLACK}████╔╝██║██║███╗██║██║       ██║   ██║██╔═══╝ ╚════██║ ${RESET}"
-        echo -e "\t\t ${GREEN}${BG_BLACK}╚██████╔╝╚███╔███╔╝███████╗  ╚██████╔╝██║     ███████║ ${RESET}"
-        echo -e "\t\t ${GREEN}${BG_BLACK} ╚═════╝  ╚══╝╚══╝ ╚══════╝   ╚═════╝ ╚═╝     ╚══════╝ ${RESET}"
+        echo -e "\t\t ${RED}${BG_BLACK} ██████╗ ██╗    ██╗██╗        ██████╗ ██████╗ ███████╗ ${RESET}"
+        echo -e "\t\t ${RED}${BG_BLACK}██╔═████╗██║    ██║██║       ██╔═══██╗██╔══██╗██╔════╝ ${RESET}"
+        echo -e "\t\t ${RED}${BG_BLACK}██║██╔██║██║ █╗ ██║██║       ██║   ██║██████╔╝███████╗ ${RESET}"
+        echo -e "\t\t ${RED}${BG_BLACK}████╔╝██║██║███╗██║██║       ██║   ██║██╔═══╝ ╚════██║ ${RESET}"
+        echo -e "\t\t ${RED}${BG_BLACK}╚██████╔╝╚███╔███╔╝███████╗  ╚██████╔╝██║     ███████║ ${RESET}"
+        echo -e "\t\t ${RED}${BG_BLACK} ╚═════╝  ╚══╝╚══╝ ╚══════╝   ╚═════╝ ╚═╝     ╚══════╝ ${RESET}"
         author_version;
     }
 
     # Function: Display the menu header with the script name and author
     function author_version() {
-        echo -e "\t\t ${BG_BLACK}${MAGENTA} 0wL Operators Script (0wLOpS) v$VERSION ${GRAY}by $AUTHOR ${RESET}"
+        echo -e "\t\t ${BG_RED}${RED} 0wL Operators Script (0wLOpS) v$VERSION ${GRAY}by $AUTHOR ${RESET}"
         subtitle;
     }
 
