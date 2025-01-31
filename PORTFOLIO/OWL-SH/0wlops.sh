585b291ab4a4822a3fccb167589a17f0f403255a
JPGress <jpgress@gmail.com>
2025-01-31 15:55:59 -0300

    Bump version to 0.21.4 and update nmap interface selection handling

--- PORTFOLIO/OWL-SH/0wlops.sh
+++ PORTFOLIO/OWL-SH/0wlops.sh
@@ -3,7 +3,7 @@
 # TODO: Extracting URLs from a Web Page - Web and Internet Users (177) - Chapter 7 - Wicked Cool Scripts
 
 # Version
-VERSION="0.21.3"
+VERSION="0.21.4"
 # Darth Release
 RELEASE="ANAKIN"
 #* ====== CONSTANTS ======
@@ -230,7 +230,7 @@ RELEASE="ANAKIN"
         echo -e "${BLACK} ${BG_RED} INVALID OPTION!${RESET}${RED} - Please run the script again and choose a valid option.${RESET}"
         echo -e "${GRAY} Press ENTER to continue ${RESET}"
         read -r
-        main_menu
+        main
     }
 
     function log_and_display() {
@@ -3053,7 +3053,6 @@ RELEASE="ANAKIN"
             if [[ ! "$interface_num" =~ ^[0-9]+$ ]] || ((interface_num < 1 || interface_num > total_interfaces)); then
                 log_and_display_no_date "---"
                 log_and_display_no_date "$(invalid_option)"
-                pause_script
                 nmap_discovery_workflow
             fi
 
